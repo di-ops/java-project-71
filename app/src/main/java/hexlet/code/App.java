b2c4b09a7a250c7107d229fb206daf0a24cd70e2
Dmitry Vekshin <vekshin8@gmail.com>
2022-10-02 22:07:02 +0300

    Add option -f(--format) and parameters filePath1 and filePath2

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,12 +1,25 @@
 package hexlet.code;
 
 import picocli.CommandLine;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
 import picocli.CommandLine.Command;
 import java.util.concurrent.Callable;
 
 @Command(name = "gendiff", mixinStandardHelpOptions = true, version = "1.0",
         description = "Compares two configuration files and shows a difference.")
 public class App implements Callable<Integer> {
+
+    @Option(names = {"-f", "--format"}, description = "output format: [default: ${DEFAULT-VALUE}]",
+            defaultValue = "stylish", paramLabel = "format")
+    private String format;
+
+    @Parameters(index = "0", description = "path to first file", paramLabel = "filePath1")
+    private String filePath1;
+
+    @Parameters(index = "1", description = "path to second file", paramLabel = "filePath2")
+    private String filePath2;
+
 
     @Override
     public Integer call() throws Exception {
